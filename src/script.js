d2acc420ecae7a9b60f203a37001859a1733eff0
Favour Emmanuel <iyenefavour@gmail.com>
2023-12-31 13:16:17 +0100

    created the script file

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menuToggle");
-  const menu = document.getElementById("menu");
+const hamburgerIcon = document.querySelector("#menuToggler");
+const navMenuIcon = document.querySelector("#menuBar");
 
-  menuToggle.addEventListener("click", function () {
-    menu.classList.toggle("hidden");
-  });
+hamburgerIcon.addEventListener("click", () => {
+  // alert('hello')
+  hamburgerIcon.classList.toggle("active");
+  navMenuIcon.classList.toggle("active");
 });
+
+document.querySelectorAll(".navList").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburgerIcon.classList.remove("active");
+    navMenuIcon.classList.remove("active");
+  })
+);
