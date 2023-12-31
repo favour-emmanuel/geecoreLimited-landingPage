5b957d2510ce15d9263748818590adc6a418781f
Favour Emmanuel <iyenefavour@gmail.com>
2023-12-31 01:13:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        appBlack: "#212529",
+        appYellow: "#F2C944",
+        paleOrange: "#FDF8E8",
+        appGray: "#4D5154",
+        lightGray: "#999B9D",
+        appPink: "#FDF8E8",
+        appSalmon: "#FAEAB6",
+      },
+      screens: {
+        myScreen: "2000px",
+      },
+    },
+  },
+  plugins: [],
+};
